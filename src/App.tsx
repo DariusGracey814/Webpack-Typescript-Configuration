e20499782ed00a7e630ff5f7fdf5b332e9b400c3
DariusGracey814 <Dariusgracey814@gmail.com>
2023-11-05 15:18:47 -0500

    steps 8-11

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Welcome Back to React Darius we missed you</h1>;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDom from "react-dom";
+import App from "./App";
+
+// Inject React App into DOM root element
+ReactDom.render(<App />, document.getElementById("root"));
